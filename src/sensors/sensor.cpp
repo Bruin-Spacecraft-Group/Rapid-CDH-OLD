f8f88e28eb3124b2ce6a79f5b8107b7537a245ab
Tiffany <tiffany9342@gmail.com>
2021-12-01 18:42:14 -0800

    Added sensor classes from SPARTAN

---
+++ src/sensors/sensor.cpp
@@ -0,0 +1,20 @@
+#include "sensor.h"
+
+// Constructor
+Sensor::Sensor(int busID, int instance)
+        : m_busID(busID), m_status(STATUS_OFF), m_instance(instance) {}
+
+// Debug output
+void Sensor::printEscapedValues(bool normalize) const {
+    int lines = printValues();
+    std::cout << "\033[100D" << std::flush;
+    std::cout << "\033[" << lines << "A" << std::flush;
+    if (normalize) {
+        std::cout << "\033[" << lines << "B" << std::flush;
+    }
+}
+
+// Generic function implementation for the sensor class
+int Sensor::getBusID() const { return m_busID; }
+int Sensor::getStatus() const { return m_status; }
+int Sensor::getInstance() const { return m_instance; }
