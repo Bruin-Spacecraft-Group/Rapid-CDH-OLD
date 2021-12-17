e9963913153ffdae8ad6aa912cbe93d066f5d873
tiffany1618 <tiffany9342@gmail.com>
2021-12-16 23:45:24 -0800

    Fix tabs

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,13 @@
 void foo() {}
 
 int main() {
-  std::cout << "starting up" << std::endl;
+    std::cout << "starting up" << std::endl;
 
-  while (true) {
+    while (true) {
 
 
 
     // sleep for 50 ms
     std::this_thread::sleep_for(std::chrono::milliseconds(50));
-  }
+    }
 }
