89bc0d07646f8d9151bc3357bc5b630f98309f56
tiffany1618 <tiffany9342@gmail.com>
2021-12-24 11:17:47 -0800

    Add some helper functions

--- src/main.cpp
+++ src/main.cpp
@@ -12,26 +12,19 @@ using std::cout;
 using std::cerr;
 using std::endl;
 
-int main() {
-    if (wiringPiSetup() == -1) {
-        cerr << "Unable to start WiringPi" << endl;
-        return 1;
-    } else {
-        cout << "WiringPi set up" << endl;
-    }
-
+void ucam_get_raw() {
     std::ofstream fout;
+    UCamIII ucam(SERIAL_DEV_0, SERIAL_BAUD_RATE, UCAM_RESET_PIN,
+                 UCamIII::FMT_RAW_RGB_16, UCamIII::RAW_160x120, fout);
 
-    // Get a RAW image
-//    UCamIII ucam(SERIAL_DEV_0, SERIAL_BAUD_RATE, UCAM_RESET_PIN,
-//                 UCamIII::FMT_RAW_RGB_16, UCamIII::RAW_160x120, fout);
-//
-//    ucam.init();
-//    ucam.snapshot(UCamIII::SNAP_RAW);
-//    uint32_t len = ucam.get_picture(UCamIII::PIC_SNAPSHOT);
-//    ucam.write_raw_data(len);
+    ucam.init();
+    ucam.snapshot(UCamIII::SNAP_RAW);
+    uint32_t len = ucam.get_picture(UCamIII::PIC_SNAPSHOT);
+    ucam.write_raw_data(len);
+}
 
-    // Get a JPEG image
+void ucam_get_jpeg() {
+    std::ofstream fout;
     UCamIII ucam(SERIAL_DEV_0, SERIAL_BAUD_RATE, UCAM_RESET_PIN,
                  UCamIII::FMT_JPEG, UCamIII::JPEG_640x480, fout);
 
@@ -40,6 +33,15 @@ int main() {
     ucam.snapshot(UCamIII::SNAP_JPEG);
     uint32_t len = ucam.get_picture(UCamIII::PIC_SNAPSHOT);
     ucam.write_jpeg_data(len);
+}
+
+int main() {
+    if (wiringPiSetup() == -1) {
+        cerr << "Unable to start WiringPi" << endl;
+        return 1;
+    } else {
+        cout << "WiringPi set up" << endl;
+    }
 
     return 0;
 }
