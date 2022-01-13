cc219018457493ad683fe224a983ff26ed428076
Randall Scharpf <randall.scharpf@gmail.com>
2022-01-13 12:11:21 -0800

    switch the PPG102A6 RTD to use an ADC in voltage divider configuration with gpio on/off

--- src/sensors/PPG102A6.cpp
+++ src/sensors/PPG102A6.cpp
@@ -1,11 +1,33 @@
 #include "PPG102A6.h"
 
-#include "ina260.h"
+#include <wiringPi.h>
 
-PPG102A6::PPG102A6(ina260* sensor) : resistanceAtZero(1000), ppmPerDegree(3850), sensor(sensor) {}
+PPG102A6::PPG102A6(ADS7828* sensor, int channel, int gpioPin) {
+	resistanceAtZero = 1000;
+	ppmPerDegree = 3850;
+	this->sensor = sensor;
+	// Expected resistance of the RTD at 50 degrees C.
+	// Choosing this value maximizes the minimum value for
+	// d(Voltage)/d(Temperature) on the range -40 to +50
+	// degrees C. If this exact value of resistor cannot be
+	// ordered, the real value should be used.
+	this->dividerResistance = 1192.5;
+
+	this->channel = channel;
+	this->gpioPin = gpioPin;
+
+	pinMode(gpioPin, OUTPUT);
+	pullUpDnControl(gpioPin, PUD_OFF);
+	digitalWrite(gpioPin, LOW);
+}
 
 double PPG102A6::getTemperature() {
-	// using R = R0 * (10^6 + ppm * T) and V = IR
-	double resistance = sensor->readVoltage_mV() / sensor->readCurrent_mA();
+	digitalWrite(gpioPin, HIGH);
+	double voltage = sensor->readChannelCommonAnode(channel);
+	digitalWrite(gpioPin, LOW);
+	// using V = IR
+	double current = voltage / dividerResistance;
+	double resistance = voltage / current;
+	// using R = R0 * (10^6 + ppm * T)
 	return (resistance / resistanceAtZero - 1000000) / ppmPerDegree;
 }
--- src/sensors/PPG102A6.h
+++ src/sensors/PPG102A6.h
@@ -5,12 +5,15 @@ class ina260;
 
 class PPG102A6 {
     public:
-        PPG102A6(ina260* sensor);
+        PPG102A6(ADS7828* sensor, int channel, int gpioPin);
         double getTemperature();
     private:
         double resistanceAtZero;
         double ppmPerDegree;
-        ina260* sensor;
+        ADS7828* sensor;
+        double dividerResistance;
+        int channel;
+        int gpioPin;
 };
 
 #endif
