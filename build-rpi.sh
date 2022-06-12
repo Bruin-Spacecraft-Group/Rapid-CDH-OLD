9877cb6e3998fd9e2b53261325f7518fec22d339
Tiffany Chieu <tiffany9342@gmail.com>
2022-06-11 21:56:22 -0700

    Update cross-compilation scripts

--- build-rpi.sh
+++ build-rpi.sh
@@ -2,6 +2,15 @@
 
 # Build script for cross-compiling for Raspberry Pi
 
+# Get rpi rootfs directory
+ROOTFS_DIR=""
+while getopts 'd:' flag; do
+  case "${flag}" in
+    d) ROOTFS_DIR="${OPTARG}" ;;
+    *) exit 1 ;;
+  esac
+done
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [ ! -d $DIR/build ]; then
@@ -13,7 +22,7 @@ cd $DIR/build
 # To get the rootfs which is required here, use:
 # rsync -rl --delete-after --safe-links pi@192.168.1.PI:/{lib,usr} $HOME/rpi/rootfs
 
-export RASPBIAN_ROOTFS=$HOME/rpi/rootfs
+export RASPBIAN_ROOTFS=$ROOTFS_DIR/rpi/rootfs
 export PATH=/opt/cross-pi-gcc/bin:/opt/cross-pi-gcc/libexec/gcc/arm-linux-gnueabihf/8.3.0:$PATH
 export RASPBERRY_VERSION=1
 
--- setup-rpi.sh
+++ setup-rpi.sh
@@ -2,6 +2,15 @@
 
 # Setup script for cross-compiling for Raspberry Pi
 
+# Get rpi rootfs directory
+ROOTFS_DIR=""
+while getopts 'd:' flag; do
+  case "${flag}" in
+    d) ROOTFS_DIR="${OPTARG}" ;;
+    *) exit 1 ;;
+  esac
+done
+
 # Find CMake version
 CMAKE_VERSION=""
 if cmake --version > /dev/null ; then
@@ -12,33 +21,66 @@ else
   exit 1
 fi
 
+# Find CMake modules directory
+CMAKE_MODULES_DIR=""
+if [ -d "/usr/share/cmake/Modules" ]; then
+    CMAKE_MODULES_DIR="/usr/share/cmake/Modules"
+elif [ -d "/usr/share/cmake-$CMAKE_VERSION/Modules" ]; then
+    CMAKE_MODULES_DIR="/usr/share/cmake-$CMAKE_VERSION/Modules"
+else
+    echo "CMake Modules directory not found"
+    exit 1
+fi
+
 # Create FindWiringPi.cmake in /usr/share/cmake-<version-number>/Modules
-echo -e \
-"find_library(WIRINGPI_LIBRARIES NAMES wiringPi)\n"\
-"find_path(WIRINGPI_INCLUDE_DIRS NAMES wiringPi.h)\n\n"\
-"include(FindPackageHandleStandardArgs)\n"\
-"find_package_handle_standard_args(WiringPi DEFAULT_MSG WIRINGPI_LIBRARIES WIRINGPI_INCLUDE_DIRS)"\
-> /usr/share/cmake-$CMAKE_VERSION/Modules/FindWiringPi.cmake
-echo "Created FindWiringPi.cmake in /usr/share/cmake-$CMAKE_VERSION/Modules"
+if [ -f "$CMAKE_MODULES_DIR/FindWiringPi.cmake" ]; then
+    echo "FindWiringPi.cmake exists; skipping"
+else
+    echo -e \
+    "find_library(WIRINGPI_LIBRARIES NAMES wiringPi)\n"\
+    "find_path(WIRINGPI_INCLUDE_DIRS NAMES wiringPi.h)\n\n"\
+    "include(FindPackageHandleStandardArgs)\n"\
+    "find_package_handle_standard_args(WiringPi DEFAULT_MSG WIRINGPI_LIBRARIES WIRINGPI_INCLUDE_DIRS)"\
+    > $CMAKE_MODULES_DIR/FindWiringPi.cmake
+    echo "Created FindWiringPi.cmake in $CMAKE_MODULES_DIR"
+fi
 
-echo "Getting the RPi toolchain..."
-wget https://github.com/Pro/raspi-toolchain/releases/latest/download/raspi-toolchain.tar.gz
-echo "Done"
+# Install rpi toolchain
+if [ -d "/opt/cross-pi-gcc" ]; then
+    echo "/opt/cross-pi-gcc exists; skipping"
+else
+    # Download rpi toolchain
+    if [ -f "./raspi-toolchain.tar.gz" ]; then
+        echo "raspi-toolchain.tar.gz exists; skipping"
+    else
+        echo "Getting the RPi toolchain..."
+        wget https://github.com/Pro/raspi-toolchain/releases/latest/download/raspi-toolchain.tar.gz
+        echo "Done"
+    fi
 
-echo "Installing the RPi toolchain to /opt/cross-pi-gcc..."
-tar xzvf raspi-toolchain.tar.gz --strip-components=1 -C /opt
-echo "Done"
+    echo "Installing the RPi toolchain to /opt/cross-pi-gcc..."
+    tar xzvf raspi-toolchain.tar.gz --strip-components=1 -C /opt
+    echo "Done"
+fi
 
 # Create directory for rootfs for Raspberry Pi library files
-mkdir $HOME/rpi
+if [ -d "$ROOTFS_DIR/rpi" ]; then
+    echo "$ROOTFS_DIR/rpi exists; skipping"
+else
+    # Download rpi library files
+    if [ -f "./rpi-rootfs.tar.gz" ]; then
+        echo "rpi-rootfs.tar.gz exists; skipping"
+    else
+        echo "Getting the RPi library files..."
+        wget https://github.com/Bruin-Spacecraft-Group/RapidCDH/releases/latest/download/rpi-rootfs.tar.gz
+        echo "Done"
+    fi
 
-echo "Getting the RPi library files..."
-wget https://github.com/Bruin-Spacecraft-Group/RapidCDH/releases/latest/download/rpi-rootfs.tar.gz
-echo "Done"
-
-echo "Installing the RPi library files in $HOME/rpi..."
-tar xzvf rpi-rootfs.tar.gz -C $HOME/rpi
-ln -s $HOME/rpi/rootfs/usr/lib/ $HOME/rpi/rootfs/
-echo "Done"
+    mkdir $ROOTFS_DIR/rpi
+    echo "Installing the RPi library files in $ROOTFS_DIR/rpi..."
+    tar xzvf rpi-rootfs.tar.gz -C $ROOTFS_DIR/rpi
+    ln -s $ROOTFS_DIR/rpi/rootfs/usr/lib/ $ROOTFS_DIR/rpi/rootfs/
+    echo "Done"
+fi
 
 echo "Setup finished"
